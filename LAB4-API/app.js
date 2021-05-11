8695f4a60bbc2d62bc6ec70183aa2896a58369a7
Medina Dadurgova <medina-dadurgova@hotmail.com>
2021-05-11 19:30:13 +0200

    feature: fetch weather based on location using tomorrow.io API

--- LAB4-API/app.js
+++ LAB4-API/app.js
@@ -12,9 +12,32 @@ class App {
     fetchLocationSuccess(result) {
         this.lat = result.coords.latitude;
         this.long = result.coords.longitude;
-        console.log(result);
+        let location = [this.lat, this.long];
+        this.getWeather(location);
     }
-    
+
+    getWeather(location) {
+        const baseUrl = "https://api.tomorrow.io/v4/timelines";
+        const apiKey = "UCuDjOGkeJ11ItScQ18n8ZIQA7OtbdOC";
+        const timestep = "current";
+        const field = "temperature";
+        const unit = "metric";
+        const timezone = "Europe/Brussels";
+        const url = `${baseUrl}?location=${location}&fields=${field}&timesteps=${timestep}&timezone=${timezone}&units=${unit}&apikey=${apiKey}`;
+        
+        fetch(url)
+            .then(response => {
+                console.log(response);
+                response.json;
+            })
+            .then(data => {
+                console.log(data);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
     fetchLocationFailed(err) {
         console.log(err);
     }
